53da7096500dc2befc01186449536f7a86222cd8
David <dkovel7@gmail.com>
2025-03-14 15:47:17 +0000

    Initial

--- Core/Entity/Book.cs
+++ Core/Entity/Book.cs
@@ -5,7 +5,6 @@ public class Book
     public int Id { get; set; }
     public string Title { get; set; }
     public string Author { get; set; }
-    public string ISBN { get; set; }
     public int PublicationYear { get; set; }
     public string Genre { get; set; }
     public bool IsAvailable { get; set; } = true;
@@ -14,7 +13,6 @@ public class Book
     {
         return !string.IsNullOrEmpty(Title) &&
                !string.IsNullOrEmpty(Author) &&
-               !string.IsNullOrEmpty(ISBN) &&
                PublicationYear > 0;
     }
 }
